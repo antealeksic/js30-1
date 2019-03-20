c984599e4b8ae2eeedaf5263525b477ea5ac451f
ante <ante.aleksic.sb@gmail.com>
2019-03-20 19:56:33 +0100

    js30 drums with my twist

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-window.addEventListener("keydown", function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
-    audio.currentTime = 0; //rewind to start
-    audio.play();
+
+
+function playSounds(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selected every audio element with data-key
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selected element with class of key
+    if (!audio) return;
+    audio.currentTime = 0; //rewind to start so same audio can be played after being already played
+    audio.play(); //if key with no audio is pressed function will stop running
     key.classList.add("playing");
 
-});
+}
 
-function removeTransition(e) {
-    if(e.propretyName !== "transform") return; //function to remove  transitions
+function removeTransition (e) {
+    if (e.propertyName !== "transform") return;
     this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+const keys = document.querySelectorAll(".key"); //selected all keys (with class of .key)
+keys.forEach(key => key.addEventListener("transitionend", removeTransition)); //each key god eventlistener transitionend added
+window.addEventListener("keydown", playSounds);
